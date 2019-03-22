b60b33444a2e3ac6c3703b2d74ce50f480cfca38
= <nick@viastudio.com>
2019-03-22 18:40:22 -0400

    Made intial results page

--- cis411/src/app/app-routing.module.ts
+++ cis411/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ArtPageComponent } from './art-page/art-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full'}
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'results', component: ArtPageComponent, pathMatch: 'full'}
 ];
 
 
